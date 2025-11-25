2b0d3b8c41f0ff8abee3fb0ee5ddafc1e177c3de
ilariamotta <ilaria.motta93@gmail.com>
2025-11-25 15:16:48 +0100

    feat: creazione componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import style from "./Header.module.css"
+
+export default function Header() {
+    return (
+        <header>
+            <img src="" alt=""/>
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </header>
+    )
+}

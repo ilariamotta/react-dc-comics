5df08ad01db1f82a1495cbf5d128afb4b83b5ada
ilariamotta <ilaria.motta93@gmail.com>
2025-11-26 17:36:51 +0100

    feat: stampa  dinamica navbar

--- src/assets/navlinks.js
+++ src/assets/navlinks.js
@@ -1,66 +1,65 @@
 const navlinks = [
     {
         id: 1,
-        title: characters,
-        path: /characters,
+        title: "characters",
+        path: "/characters",
         active: false,
     },
     {
         id: 2,
-        title: comics,
-        path: /comics,
+        title: "comics",
+        path: "/comics",
         active: true,
     },
     {
         id: 3,
-        title: movies,
-        path: /movies,
+        title: "movies",
+        path: "/movies",
         active: false,
     },
     {
         id: 4,
-        title: tv,
-        path: /tv,
+        title: "tv",
+        path: "/tv",
         active: false,
     },
     {
         id: 5,
-        title: games,
-        path: /games,
+        title: "games",
+        path: "/games",
         active: false,
     },
     {
         id: 6,
-        title: collectibles,
-        path: /collectibles,
+        title: "collectibles",
+        path: "/collectibles",
         active: false,
     },
     {
         id: 7,
-        title: videos,
-        path: /videos,
+        title: "videos",
+        path: "/videos",
         active: false,
     },
     {
         id: 8,
-        title: fans,
-        path: /fans,
+        title: "fans",
+        path: "/fans",
         active: false,
     },
     {
         id: 9,
-        title: news,
-        path: /news,
+        title: "news",
+        path: "/news",
         active: false,
     },
     {
         id: 10,
-        title: shop,
-        path: /shop,
+        title: "shop",
+        path: "/shop",
         active: false,
     },
 
-
-
-
 ]
+
+export {navlinks}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import "./Header.css"
 import Logo from "./Logo.jsx"
+import { navlinks } from "../assets/navlinks.js"
 
 export default function Header() {
     return (
         <section className="bg-white">
         <header className="d-flex container justify-space-between">
             <Logo/>
-            <ul className="nav-link">
-                <li><a href="" className="active">characters</a></li>
+            <ul className="d-flex">
+                {navlinks.map((curLink) => {
+                    const linkClass = curLink.active === true ? "nav-link active" : "nav-link";
+                
+                return <li key={curLink.id}><a href={curLink.path} className={linkClass}>{curLink.title}</a></li>})}
+                
 
             </ul>
         </header>

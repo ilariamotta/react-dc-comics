912c65cc842984182432b448fa9f90f8fc53c728
ilariamotta <ilaria.motta93@gmail.com>
2025-11-26 16:53:12 +0100

    feat: creazione file link navbar

---
+++ src/assets/navbar.js
@@ -0,0 +1,66 @@
+const navlinks = [
+    {
+        id: 1,
+        title: characters,
+        path: /characters,
+        active: false,
+    },
+    {
+        id: 2,
+        title: comics,
+        path: /comics,
+        active: true,
+    },
+    {
+        id: 3,
+        title: movies,
+        path: /movies,
+        active: false,
+    },
+    {
+        id: 4,
+        title: tv,
+        path: /tv,
+        active: false,
+    },
+    {
+        id: 5,
+        title: games,
+        path: /games,
+        active: false,
+    },
+    {
+        id: 6,
+        title: collectibles,
+        path: /collectibles,
+        active: false,
+    },
+    {
+        id: 7,
+        title: videos,
+        path: /videos,
+        active: false,
+    },
+    {
+        id: 8,
+        title: fans,
+        path: /fans,
+        active: false,
+    },
+    {
+        id: 9,
+        title: news,
+        path: /news,
+        active: false,
+    },
+    {
+        id: 10,
+        title: shop,
+        path: /shop,
+        active: false,
+    },
+
+
+
+
+]
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,9 @@ export default function Header() {
         <section className="bg-white">
         <header className="d-flex container justify-space-between">
             <Logo/>
-            <ul className="d-flex justify-space-between nav-link">
-                <li><a href="">characters</a></li>
-                <li><a href="">comics</a></li>
-                <li><a href="">movies</a></li>
-                <li><a href="">tv</a></li>
-                <li><a href="">games</a></li>
-                <li><a href="">collectibles</a></li>
-                <li><a href="">videos</a></li>
-                <li><a href="">fans</a></li>
-                <li><a href="">news</a></li>
-                <li><a href="">shop</a></li>
+            <ul className="nav-link">
+                <li><a href="" className="active">characters</a></li>
+
             </ul>
         </header>
         </section>
